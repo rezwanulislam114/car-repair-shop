00b30c00d27524b9c7c1dbc4efa8795e90ad6b26
rezwanulislam114 <rezwanulislam114@gmail.com>
2021-10-14 21:35:42 +0600

    footer added

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -6,9 +6,9 @@ const Footer = () => {
     return (
         <div className="text-center bg-dark text-white pt-4 pb-2">
             <h3>Follow Us</h3>
-            <Link>Facebook</Link>
-            <Link>WhatsApp</Link>
-            <Link>Twitter</Link>
+            <Link className="footer-link" to="/home">Facebook</Link>
+            <Link className="footer-link" to="/home">WhatsApp</Link>
+            <Link className="footer-link" to="/home">Twitter</Link>
             <p className="mb-0 mt-3">copyright <span className="text-danger">RED</span>WILD @2021</p>
         </div>
     );
